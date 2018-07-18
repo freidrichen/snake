f144233103336597eedfa2be55998e4dc2eb738a
Magnus Sandén <thefreid@gmail.com>
2018-07-19 00:18:32 +0200

    Fixed size snake. Models steps on key-press.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 use std::time::Duration;
+use std::collections::VecDeque;
 
 
 const SQUARE_SIZE: u32 = 20;
@@ -19,8 +20,31 @@ const FULL_WINDOW_HEIGHT: u32 = SQUARE_SIZE*PLAYGROUND_HEIGHT + 2*BORDER_SIZE;
 const FULL_WINDOW_WIDTH: u32 = SQUARE_SIZE*PLAYGROUND_WIDTH + 2*BORDER_SIZE;
 
 
-type GameState = (i32, i32);
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
 
+type Tile = (i32, i32);
+struct GameState {
+    snake: VecDeque<Tile>,
+    length: u32,
+    direction: Direction,
+}
+
+impl GameState {
+    fn new(start_tile: Tile) -> GameState {
+        let mut state = GameState {
+            snake: VecDeque::new(),
+            length: 10,
+            direction: Direction::Right,
+        };
+        state.snake.push_front(start_tile);
+        state
+    }
+}
 
 fn to_screen_coords(game_x: i32, game_y: i32) -> (i32, i32) {
     let screen_x = BORDER_SIZE as i32 + game_x*(SQUARE_SIZE as i32);
@@ -37,13 +61,26 @@ fn draw(game_state: &GameState, canvas: &mut Canvas<Window>) {
     canvas.fill_rect(Rect::new(BORDER_SIZE as i32, BORDER_SIZE as i32,
                                PLAYGROUND_WINDOW_WIDTH, PLAYGROUND_WINDOW_HEIGHT)).unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    let screen_coords = to_screen_coords(game_state.0, game_state.1);
-    canvas.fill_rect(Rect::new(screen_coords.0, screen_coords.1,
-                               SQUARE_SIZE, SQUARE_SIZE)).unwrap();
+    for tile in &game_state.snake {
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        let screen_coords = to_screen_coords(tile.0, tile.1);
+        canvas.fill_rect(Rect::new(screen_coords.0, screen_coords.1,
+                                   SQUARE_SIZE, SQUARE_SIZE)).unwrap();
+    }
 
     canvas.present();
 }
+
+
+fn move_(game_state: &mut GameState, dx: i32, dy: i32) {
+    let new_head = (game_state.snake.front().unwrap().0 + dx,
+                    game_state.snake.front().unwrap().1 + dy);
+    game_state.snake.push_front(new_head);
+    if game_state.snake.len() as u32 > game_state.length {
+        game_state.snake.pop_back();
+    }
+}
+
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -59,17 +96,17 @@ fn main() {
         .unwrap();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut game_state = (0, 0);
+    let mut game_state = GameState::new((0, 0));
 
     'main_loop: loop {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} => break 'main_loop,
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'main_loop,
-                Event::KeyDown { keycode: Some(Keycode::Up), .. } => game_state.1 -= 1,
-                Event::KeyDown { keycode: Some(Keycode::Down), .. } => game_state.1 += 1,
-                Event::KeyDown { keycode: Some(Keycode::Left), .. } => game_state.0 -= 1,
-                Event::KeyDown { keycode: Some(Keycode::Right), .. } => game_state.0 += 1,
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => move_(&mut game_state, 0, -1),
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => move_(&mut game_state, 0, 1),
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => move_(&mut game_state, -1, 0),
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => move_(&mut game_state, 1, 0),
                 _ => {},
             }
         }
